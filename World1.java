d3106b6d9c6aa4309cbd67ff754b745b3119a04d
Noé Cisneros <92272350+ZisnerNeo@users.noreply.github.com>
2023-06-27 22:40:02 -0600

    a

--- World1.java
+++ World1.java
@@ -16,6 +16,8 @@ public class World1 extends Screen
     
     public World1()
     {
+        GreenfootImage img = new GreenfootImage("Background-4.png");
+        setBackground(img);
         addObject(n1,120,50);
         addObject(n2,240,150);
         addObject(n3,360,250);
@@ -25,19 +27,23 @@ public class World1 extends Screen
     public void act(){
         if(Greenfoot.mouseClicked(n1)){
             Lvl1 lvl1 = new Lvl1();
-            Greenfoot.setWorld(lvl1);
+            Camera c = new Camera(lvl1, 300, 200);
+            Greenfoot.setWorld(c);
         }
         if(Greenfoot.mouseClicked(n2)){
             Lvl2 lvl2 = new Lvl2();
-            Greenfoot.setWorld(lvl2);
+            Camera c = new Camera(lvl2, 300, 200);
+            Greenfoot.setWorld(c);
         }
         if(Greenfoot.mouseClicked(n3)){
             Lvl3 lvl3 = new Lvl3();
-            Greenfoot.setWorld(lvl3);
+            Camera c = new Camera(lvl3, 300, 200);
+            Greenfoot.setWorld(c);
         }
         if(Greenfoot.mouseClicked(n4)){
             Lvl4 lvl4 = new Lvl4();
-            Greenfoot.setWorld(lvl4);
+            Camera c = new Camera(lvl4, 300, 200);
+            Greenfoot.setWorld(c);
         }
         if(Greenfoot.mouseClicked(r)){
             SubWorlds worldmenu = new SubWorlds();
